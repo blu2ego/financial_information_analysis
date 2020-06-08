0d1d7e5670e2f61c20c3320e8e3c8bcebcd87ef0
blu2ego <blu2ego@gmail.com>
2020-06-08 11:53:39 +0900

    cleaning codes

--- data_processing/extract_table_from_br.R
+++ data_processing/extract_table_from_br.R
@@ -8,6 +8,9 @@ where_audit_table <- grep(bag_search_word, br_text)
 library(tabulizer)
 br_tables <- extract_tables("~/projects/financial_information_analysis/data/business_report/dart/sample/br.pdf", 
                             pages = where_audit_table, output = "data.frame")
-br_tables_df
 
 
+audit_raw <- as.data.frame(br_tables[1])
+audit <- audit_raw[, -3]
+audit <- audit[!(audit$사업연도 == "" ), ]
+audit

d041f7d1586c4c008c41958b4a93b71125866523
blu2ego <blu2ego@gmail.com>
2020-06-30 01:43:59 +0900

    remove api key value from code

--- data_crawling/source_dart/crawl_corp_code.R
+++ data_crawling/source_dart/crawl_corp_code.R
@@ -1,3 +1,3 @@
 tmp <- tempfile()
-download.file("https://opendart.fss.or.kr/api/corpCode.xml?crtfc_key=1fde365f255f62f4b7699e1a0c01eb09386426e0", tmp)
+download.file("https://opendart.fss.or.kr/api/corpCode.xml?crtfc_key=", tmp)
 corp_code <- unzip(tmp, "~/projects/financial_information_analysis/data/corp_general_info/CORPCODE.xml")
